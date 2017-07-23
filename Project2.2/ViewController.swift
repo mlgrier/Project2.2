f446c8d836f9ea81ebd7e15b59ae3ab8b4b64839
Marco Grier <mlgrier@gmail.com>
2017-07-23 12:45:34 -0500

    added countries

--- Project2.2/ViewController.swift
+++ Project2.2/ViewController.swift
@@ -12,10 +12,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+        
     }
 
     override func didReceiveMemoryWarning() {
