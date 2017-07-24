5aafdfa13a0ae76532ef633b6fca98c10bdaab54
Marco Grier <mlgrier@gmail.com>
2017-07-23 20:30:14 -0500

    fix the flags

--- Project2.2/ViewController.swift
+++ Project2.2/ViewController.swift
@@ -6,9 +6,11 @@
 //  Copyright © 2017 mlgrier. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
@@ -29,9 +31,14 @@ class ViewController: UIViewController {
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
+        askQuestion()
+        
     }
     
     func askQuestion() {
+        //Code to make the game random
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
