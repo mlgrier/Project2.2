b3eb1c24c42839b60023a9329dd5abd1946ba25b
Marco Grier <mlgrier@gmail.com>
2017-07-23 19:54:19 -0500

    added border color

--- Project2.2/ViewController.swift
+++ Project2.2/ViewController.swift
@@ -19,6 +19,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
     }
